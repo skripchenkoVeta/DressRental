1f15a15829f406e2130a46307df25d94b45e757a
skripchenkoveta <skripchenkoVeta@gmail.com>
2021-12-19 05:59:43 +0300

    add devise user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,35 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  name                   :string
+#  surname                :string
+#  number                 :string
+#  role                   :string
+#  profileable_type       :string
+#  profileable_id         :bigint
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   belongs_to :profileable, polymorphic: true
+
+  after_initialize :init
+
+  def init
+    self.profileable  ||= BuyerInfo.create()         
+  end
   
 end
