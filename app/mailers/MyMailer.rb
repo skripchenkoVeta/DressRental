63bd31afefa692340344c3415e4e65b761afc00b
skripchenkoveta <skripchenkoVeta@gmail.com>
2021-12-24 22:28:42 +0300

    use rubocop for MyMailer

--- app/mailers/MyMailer.rb
+++ app/mailers/MyMailer.rb
@@ -1,15 +1,17 @@
-class MyMailer < Devise::Mailer   
-    helper :application # gives access to all helpers defined within `application_helper`.
-    include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
-    default template_path: 'devise/mailer' # to make sure that your mailer uses the devise views
-    # If there is an object in your application that returns a contact email, you can use it as follows
-    # Note that Devise passes a Devise::Mailer object to your proc, hence the parameter throwaway (*).
-    default from: ->(*) { Class.instance.email_address }
+# frozen_string_literal: true
 
-    def confirmation_instructions(record, token, opts={})
-        headers["Custom-header"] = "Bar"
-        opts[:from] = 'my_custom_from@domain.com'
-        opts[:reply_to] = 'my_custom_from@domain.com'
-        super
-      end
+class MyMailer < Devise::Mailer
+  helper :application # gives access to all helpers defined within `application_helper`.
+  include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
+  default template_path: 'devise/mailer' # to make sure that your mailer uses the devise views
+  # If there is an object in your application that returns a contact email, you can use it as follows
+  # Note that Devise passes a Devise::Mailer object to your proc, hence the parameter throwaway (*).
+  default from: ->(*) { Class.instance.email_address }
+
+  def confirmation_instructions(record, token, opts = {})
+    headers['Custom-header'] = 'Bar'
+    opts[:from] = 'my_custom_from@domain.com'
+    opts[:reply_to] = 'my_custom_from@domain.com'
+    super
   end
+end
