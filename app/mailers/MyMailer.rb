6c0f49f0a85199e9f022020edb2858a72aae691b
skripchenkoveta <skripchenkoVeta@gmail.com>
2021-12-24 22:44:35 +0300

    fix error in MyMailer

--- app/mailers/MyMailer.rb
+++ app/mailers/MyMailer.rb
@@ -6,12 +6,4 @@ class MyMailer < Devise::Mailer
   default template_path: 'devise/mailer' # to make sure that your mailer uses the devise views
   # If there is an object in your application that returns a contact email, you can use it as follows
   # Note that Devise passes a Devise::Mailer object to your proc, hence the parameter throwaway (*).
-  default from: ->(*) { Class.instance.email_address }
-
-  def confirmation_instructions(record, token, opts = {})
-    headers['Custom-header'] = 'Bar'
-    opts[:from] = 'my_custom_from@domain.com'
-    opts[:reply_to] = 'my_custom_from@domain.com'
-    super
-  end
 end
