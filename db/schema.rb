e4c0678bfad62762f1ffbb6ea61c33a0ecb49b23
skripchenkoveta <skripchenkoVeta@gmail.com>
2022-02-14 22:52:03 +0300

    save changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_22_181033) do
+ActiveRecord::Schema.define(version: 2022_01_08_135112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,19 @@ ActiveRecord::Schema.define(version: 2021_12_22_181033) do
     t.index ["salon_id"], name: "index_products_on_salon_id"
   end
 
+  create_table "rentals", force: :cascade do |t|
+    t.datetime "start_rent"
+    t.datetime "end_rent"
+    t.boolean "status"
+    t.boolean "approve"
+    t.bigint "buyer_info_id"
+    t.bigint "product_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["buyer_info_id"], name: "index_rentals_on_buyer_info_id"
+    t.index ["product_id"], name: "index_rentals_on_product_id"
+  end
+
   create_table "salons", force: :cascade do |t|
     t.string "name"
     t.text "address"
@@ -130,6 +143,12 @@ ActiveRecord::Schema.define(version: 2021_12_22_181033) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "provider"
+    t.string "uid"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["profileable_type", "profileable_id"], name: "index_users_on_profileable"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
